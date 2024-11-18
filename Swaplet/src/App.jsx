b21dbe76fcbe5a960d6da53f67cc9804667ded85
Aditya <adityadevansh2002@gmail.com>
2024-11-18 22:02:47 +0530

    first commit

---
+++ Swaplet/src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+import './App.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Layout from './Components/Layout';
+import Home from './Components/Pages/Home';
+import LoginAndRegister from './Components/Pages/LoginAndRegister';
+
+
+function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      // errorElement: <NotFound />,
+      children: [
+        {
+          path: "/",
+          element: <Home />
+        },
+      ]
+    },
+    {
+      path: "/LoginAndRegister",
+      element: <LoginAndRegister />
+    }
+  ]);
+
+  return (
+    <RouterProvider router={router} />
+  )
+}
+
+export default App
